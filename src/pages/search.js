baadd774a957863678126d6c4b1a2f147745b29d
Kim-Hyunhee <tta_bi@naver.com>
2024-09-26 11:05:35 +0900

    Update: css, js 분리

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "../fonts/fonts.css";
 import SpaceContainer from "../components/space";
+import styles from "./search.module.css";
 
 function Search() {
   return (
@@ -13,50 +13,14 @@ function Search() {
 }
 
 function Title() {
-  return (
-    <div
-      style={{
-        position: "absolute",
-        left: "104px",
-        top: "153px",
-        fontFamily: "Noto Sans KR",
-        fontSize: "48px",
-        fontWeight: 500,
-      }}
-    >
-      <p style={{ margin: 0 }}>검색결과</p>
-    </div>
-  );
+  return <div className={styles.title}>검색결과</div>;
 }
 
 function CreateSpaceButton({ isLoggedIn }) {
   if (!isLoggedIn) {
     return null;
   }
-  return (
-    <button
-      style={{
-        position: "absolute",
-        left: "1232px",
-        top: "225px",
-        width: "154px",
-        height: "43px",
-        backgroundColor: "#FBBF24",
-        color: "white",
-        border: "none",
-        borderRadius: "6px",
-        fontFamily: "Noto Sans KR",
-        fontSize: "16px",
-        fontWeight: 500,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        cursor: "pointer",
-      }}
-    >
-      팀 스페이스 생성
-    </button>
-  );
+  return <button className={styles.createSpaceBtn}>팀 스페이스 생성</button>;
 }
 
 export default Search;
